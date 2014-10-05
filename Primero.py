d2feefac34280d142d8eb0a2750368c986f57a01
jbarcalalorenzo <barcalalor@gmail.com>
2014-10-05 02:13:19 +0200

    Cambio en variable

--- Primero.py
+++ Primero.py
@@ -4,7 +4,7 @@ import tkFileDialog
 from Menu import *
 
 class Primero(QtGui.QDialog):
-    archivo = ""
+    archivo = None
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self,parent)
         self.ui =Ui_Dialog()
