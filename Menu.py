9c9d01c34de8786cc1ac2a27bcd92f19f3e0a68a
jbarcalalorenzo <barcalalor@gmail.com>
2014-10-05 20:31:00 +0200

    Cambio en el interface

--- Menu.py
+++ Menu.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'Menu.ui'
 #
-# Created: Sun Oct 05 03:37:16 2014
+# Created: Sun Oct 05 20:30:25 2014
 #      by: PyQt4 UI code generator 4.11.2
 #
 # WARNING! All changes made in this file will be lost!
@@ -57,14 +57,29 @@ class Ui_Dialog(object):
         self.errores.setGeometry(QtCore.QRect(720, 70, 46, 13))
         self.errores.setText(_fromUtf8(""))
         self.errores.setObjectName(_fromUtf8("errores"))
+        self.skeyword = QtGui.QTextEdit(Dialog)
+        self.skeyword.setGeometry(QtCore.QRect(670, 120, 104, 16))
+        self.skeyword.setObjectName(_fromUtf8("skeyword"))
+        self.label_3 = QtGui.QLabel(Dialog)
+        self.label_3.setGeometry(QtCore.QRect(620, 120, 51, 16))
+        self.label_3.setObjectName(_fromUtf8("label_3"))
+        self.label_4 = QtGui.QLabel(Dialog)
+        self.label_4.setGeometry(QtCore.QRect(620, 90, 121, 16))
+        self.label_4.setObjectName(_fromUtf8("label_4"))
+        self.pkeyword = QtGui.QLabel(Dialog)
+        self.pkeyword.setGeometry(QtCore.QRect(750, 90, 46, 13))
+        self.pkeyword.setText(_fromUtf8(""))
+        self.pkeyword.setObjectName(_fromUtf8("pkeyword"))
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog", None))
+        Dialog.setWindowTitle(_translate("Dialog", "QA Article", None))
         self.pushButton.setText(_translate("Dialog", "Validar", None))
         self.label.setText(_translate("Dialog", "Número de Palabras :", None))
         self.toolButton.setText(_translate("Dialog", "...", None))
         self.label_2.setText(_translate("Dialog", "Número de Errores :", None))
+        self.label_3.setText(_translate("Dialog", "Keyword : ", None))
+        self.label_4.setText(_translate("Dialog", "Porcentaje de Keyword :", None))
 
